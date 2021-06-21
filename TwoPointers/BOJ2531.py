c049c9078bb43218024ec1990eab2e7d0e49bc63
hbhb0311 <gpqls9712@gmail.com>
2021-06-21 18:29:33 +0900

    백준 - 회전 초밥

--- TwoPointers/BOJ2531.py
+++ TwoPointers/BOJ2531.py
@@ -23,3 +23,34 @@ for i in range(N):
     end += 1
 
 print(cnt)
+
+
+
+
+
+##### 슬라이딩 윈도우 ??
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, d, k, coupon = map(int, input().split())
+cnt = 0
+dish = []
+
+for _ in range(N):
+    dish.append(int(input()))
+
+for i in range(k - 1):
+    dish.append(dish[i])
+
+
+window = deque(dish[0 : k])
+for i in range(N - 1):
+    window.append(coupon)
+    cnt = max(cnt, len(set(window)))
+    window.pop()
+    window.popleft()
+    window.append(dish[i + k])
+
+print(cnt)
