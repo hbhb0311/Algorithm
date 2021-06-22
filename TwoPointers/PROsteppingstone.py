bd5cb9e136f9d9c35b584f6d72322ec293515ce1
hbhb0311 <gpqls9712@gmail.com>
2021-06-22 19:12:56 +0900

    프로그래머스 - 징검다리 건너기

--- TwoPointers/PROsteppingstone.py
+++ TwoPointers/PROsteppingstone.py
@@ -1,4 +1,31 @@
-# 효율성 테스트 실패
+####### 슬라이딩 윈도우로 풀이!
+
+from collections import deque
+
+def solution(stones, k):
+    window = deque()
+    result = []
+
+    for i in range(len(stones)):
+        while window and window[-1][1] < stones[i]:
+            window.pop()
+        window.append((i, stones[i]))
+
+        if i >= k and window[0][0] == i - k:
+            window.popleft()
+
+        if i >= k - 1:
+            result.append(window[0][1])
+
+    return min(result)
+
+print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3))
+
+
+
+
+
+############# 효율성 테스트 실패
 
 from collections import deque
 
