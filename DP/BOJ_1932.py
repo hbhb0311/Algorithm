e61a5cb8bd134bd47f09e92e9e1e0d34bfd7ba75
hbhb0311 <gpqls9712@gmail.com>
2021-12-23 05:03:19 +0900

    백준 - 정수 삼각형

---
+++ DP/BOJ_1932.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+tri = []
+
+for _ in range(n):
+    tri.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(len(tri[i])):
+        if j == 0:
+            tri[i][j] += tri[i-1][j]
+        elif j == len(tri[i]) - 1:
+            tri[i][j] += tri[i-1][j-1]
+        else:
+            temp = max(tri[i-1][j-1], tri[i-1][j])
+            tri[i][j] += temp
+print(max(tri[-1]))
