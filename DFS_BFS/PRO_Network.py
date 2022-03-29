ad1f13555a8295ab3e64b568ce9f8cfd89dbf232
gpqls9712 <gpqls9712@gmail.com>
2022-03-29 14:27:41 +0900

    프로그래머스 - 네트워크

---
+++ DFS_BFS/PRO_Network.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+def dfs(num, graph, visited):
+    visited[num] = True
+    if num not in graph.keys(): return
+
+    for i in graph[num]:
+        if not visited[i]:
+            dfs(i, graph, visited)
+
+def solution(n, computers):
+    graph = defaultdict(list)    
+    visited = [False] * n
+    answer = 0
+    for idx in range(n):
+        for i, v in enumerate(computers[idx]):
+            if idx == i: continue
+            if v == 1: graph[idx].append(i)
+
+    while visited.count(False) > 0:
+        dfs(visited.index(False), graph, visited)
+        answer += 1
+
+    return answer
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
